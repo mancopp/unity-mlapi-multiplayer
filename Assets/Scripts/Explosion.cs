74053026f9ef43fc62bc0a44457d5cb48e8a24d9
Mancopp <mancoppor@gmail.com>
2022-12-13 14:55:45 +0100

    Additional mechanics

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerRespawn : NetworkBehaviour
-{
+public class Explosion : MonoBehaviour {
+
     public float cubeSize = 0.2f;
     public int cubesInRow = 5;
 
@@ -14,86 +13,25 @@ public class PlayerRespawn : NetworkBehaviour
     public float explosionForce = 50f;
     public float explosionRadius = 4f;
     public float explosionUpward = 0.4f;
-    
-    
-    private CharacterController characterController;
-    private Renderer[] renderers;
-    [SerializeField]
-    public Behaviour[] behaviours;
 
-    private PlayerHealth _health;
-    [SerializeField] private GameObject _explosionPrefab;
-
-    private List<Vector3> spawnPoints = new List<Vector3>()
-    {
-        new Vector3((float)-0.5, -1, -23),
-        new Vector3((float)-0.5, -1, 23)
-    };
-    
-    void Start()
-    {
+    // Use this for initialization
+    void Start() {
         //calculate pivot distance
         cubesPivotDistance = cubeSize * cubesInRow / 2;
         //use this value to create pivot vector)
         cubesPivot = new Vector3(cubesPivotDistance, cubesPivotDistance, cubesPivotDistance);
-        
-        characterController = GetComponent<CharacterController>();
-        renderers = GetComponentsInChildren<Renderer>();
-        _health = GetComponent<PlayerHealth>();
+
     }
 
-    public void RespawnOnServer()
-    {
-        Debug.Log("Respawn Server");
-        StartCoroutine(RespawnCoroutine());
-    }
-
-    IEnumerator RespawnCoroutine()
-    {
-        System.Random r = new System.Random();
-        HidePlayerClientRpc();
-        yield return new WaitForSeconds(3);
-        _health.ResetHP();
-        RelocatePlayerClientRpc(spawnPoints[r.Next(spawnPoints.Count)]);
-        ShowPlayerClientRpc();
-    }
-
-    [ClientRpc]
-    void HidePlayerClientRpc()
-    {
-        explode();
-        characterController.enabled = false;
-        SetPlayerState(false);
-    }
-    
-    [ClientRpc]
-    void RelocatePlayerClientRpc(Vector3 pos)
-    {
-        System.Random r = new System.Random();
-        transform.position = pos;
-    }
-    
-    [ClientRpc]
-    void ShowPlayerClientRpc()
-    {
-        SetPlayerState(true);
-        characterController.enabled = true;
-    }
-    
-    void SetPlayerState(bool state)
-    {
-        foreach (var script in behaviours)
-        {
-            script.enabled = state;
-        }
-
-        foreach (var renderer in renderers)
-        {
-            renderer.enabled = state;
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "Floor") {
+            explode();
         }
     }
-    
+
     public void explode() {
+        //make object disappear
+        gameObject.SetActive(false);
 
         //loop 3 times to create 5x5x5 pieces in x,y,z coordinates
         for (int x = 0; x < cubesInRow; x++) {
@@ -134,4 +72,5 @@ public class PlayerRespawn : NetworkBehaviour
         piece.AddComponent<Rigidbody>();
         piece.GetComponent<Rigidbody>().mass = cubeSize;
     }
+
 }
